9e436a6412465d6834a51a028930ce22472c554a
Philipp <johnyten@hotmail.com>
2023-03-15 22:04:33 +0100

    added tasks 1 and 2

---
+++ Task 1/main.py
@@ -0,0 +1,3 @@
+
+## 1. Task: Write a command line program that prints the string "Hello, World!" including a line break (newline) at the end.
+print('Hello, World!\n')
---
+++ Task 2/main.py
@@ -0,0 +1,18 @@
+
+## 2.Task: 
+def encode(firstString,offset,secondString):
+    stringList = list(firstString)
+    secondList = list(secondString)
+    asciiString = []
+    index = 1
+    
+    for charact in secondList:
+        stringList.insert(index,charact)
+        index = index + 2
+
+    print(stringList)
+
+def decode():
+    print('do that')
+
+encode('abcde',5,'jjh')
